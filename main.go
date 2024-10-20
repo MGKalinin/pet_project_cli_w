378bef798aa3acd5e9c951cf48c2e69d9131a3a6
MG_Kalinin <1953115@gmail.com>
2024-10-20 15:38:07 +0300

    redact

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ type Weather struct {
 	Location struct {
 		Name    string `json:"name"`
 		Country string `json:"country"`
-	} `json:location`
+	} `json:"location"`
 	Current struct {
 		Temp_c    float64 `json:"temp_c"`
 		Condition struct {
-			Text string `json:text`
+			Text string `json:"text"`
 		} `json:"condition"`
-	} `json:current`
+	} `json:"current"`
 }
 
 func main() {
