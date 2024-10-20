4b1c409128a2be1aa399d33b9eab26e475a4993a
MG_Kalinin <1953115@gmail.com>
2024-10-20 15:35:06 +0300

    add printf

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
+
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:location`
+	Current struct {
+		Temp_c    float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:text`
+		} `json:"condition"`
+	} `json:current`
+}
 
 func main() {
 	fmt.Println("Ready to work")
@@ -20,7 +34,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(body))
+	// fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(weather)
+	location, current :=
+		weather.Location,
+		weather.Current
+
+	fmt.Printf("%s, %s:, %.0fC, %s\n",
+		location.Country,
+		location.Name,
+		current.Temp_c,
+		current.Condition.Text)
+
 }
 
 // вытащить с json по ключам значени?
